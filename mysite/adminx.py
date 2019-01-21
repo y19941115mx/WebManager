121bd9258a6ad9e2325108c462e9a6962e0c1a1b
victor666 <1960892068@qq.com>
2019-01-21 11:11:54 +0800

    完成功能

---
+++ mysite/adminx.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+class PolicyCardAdmin(admin.ModelAdmin):
+    empty_value_display = '-空-'
+    date_hierarchy = 'add_time'
+
+    list_display = ('policy', 'title', 'apply_time', 'add_time')#要显示的字段名
+
+
+admin.site.register(PolicyCard, PolicyCardAdmin)
